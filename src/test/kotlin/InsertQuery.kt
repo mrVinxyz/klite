7d7250ea310601f9fb9171dc93fcf156bcd12f7d
mrvin <vinicius@mrvin.xyz>
2024-08-24 14:19:52 -0400

    Update tests

--- src/test/kotlin/InsertQuery.kt
+++ src/test/kotlin/InsertQuery.kt
@@ -22,7 +22,7 @@ class InsertQuery {
                 .sqlArgs()
 
         assertEquals(
-            "INSERT INTO user (id, name, email, password, record_status, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+            "INSERT INTO user (user_id, name, email, password, record_status, created_at) VALUES (?, ?, ?, ?, ?, ?)",
             sql)
 
         assertEquals(
@@ -52,7 +52,7 @@ class InsertQuery {
                 .sqlArgs()
 
         assertEquals(
-            "INSERT INTO user (id, name, email, password, record_status, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+            "INSERT INTO user (user_id, name, email, password, record_status, created_at) VALUES (?, ?, ?, ?, ?, ?)",
             sql)
 
         assertEquals(
@@ -68,13 +68,13 @@ class InsertQuery {
                 .insert(Users.id, Users.name, Users.email, Users.password)
                 .values(
                     mapOf(
-                        "id" to user.id,
+                        "user_id" to user.id,
                         "name" to user.name,
                         "email" to user.email,
                         "password" to user.password))
                 .sqlArgs()
 
-        assertEquals("INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)", sql)
+        assertEquals("INSERT INTO user (user_id, name, email, password) VALUES (?, ?, ?, ?)", sql)
 
         assertEquals(listOf(user.id, user.name, user.email, user.password), args)
     }
@@ -93,7 +93,7 @@ class InsertQuery {
                 }
                 .sqlArgs()
 
-        assertEquals("INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)", sql)
+        assertEquals("INSERT INTO user (user_id, name, email, password) VALUES (?, ?, ?, ?)", sql)
 
         assertEquals(listOf(userNull.id, userNull.name, userNull.email, userNull.password), args)
     }
@@ -118,7 +118,7 @@ class InsertQuery {
                     userNull.createdAt)
                 .sqlArgs()
 
-        assertEquals("INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)", sql)
+        assertEquals("INSERT INTO user (user_id, name, email, password) VALUES (?, ?, ?, ?)", sql)
 
         assertEquals(listOf(userNull.id, userNull.name, userNull.email, userNull.password), args)
     }
@@ -130,7 +130,7 @@ class InsertQuery {
                 .insert(Users.id, Users.name, Users.email, Users.password)
                 .values(
                     mapOf(
-                        "id" to userNull.id,
+                        "user_id" to userNull.id,
                         "name" to userNull.name,
                         "email" to userNull.email,
                         "password" to userNull.password,
@@ -138,13 +138,13 @@ class InsertQuery {
                         "created_at" to userNull.createdAt))
                 .sqlArgs()
 
-        assertEquals("INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)", sql)
+        assertEquals("INSERT INTO user (user_id, name, email, password) VALUES (?, ?, ?, ?)", sql)
 
         assertEquals(listOf(userNull.id, userNull.name, userNull.email, userNull.password), args)
     }
 
     @Test
-    fun `test sqlArgs with empty inserter`() {
+    fun `test insert empty`() {
         val (sql, args) = Inserter(Users).sqlArgs()
 
         assertEquals("INSERT INTO user () VALUES ()", sql)
--- src/test/kotlin/UpdateQuery.kt
+++ src/test/kotlin/UpdateQuery.kt
@@ -1,6 +1,8 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import query.Update
+import query.Updater
+import query.persist
+import kotlin.test.assertNotEquals
 
 class UpdateQuery {
     private val user = User(1, "John Doe", "johndoe@email.com", "johndoe123", "active", 1234567890)
@@ -9,7 +11,7 @@ class UpdateQuery {
     @Test
     fun `test update with init block`() {
         val (sql, args) =
-            Update(Users)
+            Updater(Users)
                 .update {
                     it[Users.name] = user.name
                     it[Users.email] = user.email
@@ -21,19 +23,27 @@ class UpdateQuery {
                 .sqlArgs()
 
         assertEquals(
-            "UPDATE user SET name = ?, email = ?, password = ?, record_status = ?, created_at = ? WHERE id = ?",
-            sql)
+            "UPDATE user SET name = ?, email = ?, password = ?, record_status = ?, created_at = ? WHERE user_id = ?",
+            sql,
+        )
 
         assertEquals(
             listOf(
-                user.name as Any, user.email, user.password, user.recordStatus, user.createdAt, 1),
-            args)
+                user.name as Any,
+                user.email,
+                user.password,
+                user.recordStatus,
+                user.createdAt,
+                1,
+            ),
+            args,
+        )
     }
 
     @Test
-    fun `test update with null values`() {
+    fun `test update init block with null values`() {
         val (sql, args) =
-            Update(Users)
+            Updater(Users)
                 .update {
                     it[Users.name] = userNull.name
                     it[Users.recordStatus] = userNull.recordStatus
@@ -43,10 +53,56 @@ class UpdateQuery {
                 .sqlArgs()
 
         assertEquals(
-            "UPDATE user SET name = ?, record_status = COALESCE(?, record_status), created_at = COALESCE(?, created_at) WHERE id = ?",
-            sql)
-        println(args)
+            "UPDATE user SET name = ?, record_status = COALESCE(?, record_status), created_at = COALESCE(?, created_at) WHERE user_id = ?",
+            sql,
+        )
+
         assertEquals(
-            listOf(userNull.name as Any, userNull.recordStatus, userNull.createdAt, 1), args)
+            listOf(userNull.name as Any, userNull.recordStatus, userNull.createdAt, 1),
+            args,
+        )
+    }
+
+    @Test
+    fun `test update empty`() {
+        val (sql, args) = Updater(Users).sqlArgs()
+
+        assertEquals("UPDATE user SET ", sql)
+        assertEquals(emptyList(), args)
+    }
+
+    @Test
+    fun `test update persist method`() {
+        var updateQuery =
+            Updater(Users)
+                .update {
+                    it[Users.name] = "Jhon Doe"
+                    it[Users.email] = user.email
+                    it[Users.password] = user.password
+                    it[Users.recordStatus] = user.recordStatus
+                    it[Users.createdAt] = user.createdAt
+                }
+                .where { Users.id equal 1 }
+
+        val db = Database()
+        db.conn().use {
+            createUserTable(it)
+            feedUserTable(it)
+
+            val result = updateQuery.persist(it)
+            assert(result.isSuccess)
+
+            val selectAssertQuery = "SELECT name FROM user WHERE user_id = 1"
+            it.prepareStatement(selectAssertQuery).use { stmt ->
+                val rs = stmt.executeQuery()
+                rs.next()
+
+                val updatedName = rs.getString("name")
+                assertNotEquals(user.name, updatedName)
+                assertEquals("Jhon Doe", updatedName)
+            }
+        }
+
+        db.cleanUp()
     }
 }
